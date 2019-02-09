a7fbc26e995a94658b937c6cdbff5741df17be40
Oriyo Chakma <41955723+oriyocookie@users.noreply.github.com>
2019-02-09 00:35:36 -0600

    Update GitShowRefScript.sh

--- GitShowRefScript.sh
+++ GitShowRefScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #script that Lists references in a local repository 
 #then echos the result intoa text file
-for BRANCH in `git show-ref -s |sed 's/\*//g'`;
+for BRANCH in `git show-ref -s`
 do 
 	echo "for $BRANCH:" >> result 
 	git show --name-only $BRANCH >> result
